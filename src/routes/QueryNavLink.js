592204b9961b0a81f0553d5307afd834992acc96
Franklin <frankkli.personal@gmail.com>
2022-08-25 04:03:15 -0500

    Pruebas con la version 6 de React Router Dom

---
+++ src/routes/QueryNavLink.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useLocation, NavLink } from "react-router-dom";
+
+function QueryNavLink({ to, ...props }) {
+    let location = useLocation();
+    return <NavLink to={to + location.search} {...props} />;
+  }
+
+export default QueryNavLink;
+
+
+
+
